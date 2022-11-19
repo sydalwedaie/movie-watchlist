b8dc4c783dc60cdd602ba534c1addfac2a1ccac3
Sayed Alwedaie <sydalwedaie@gmail.com>
2022-11-19 13:21:45 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Scrimba Solo Project - Movie Watchlist
+
+This is a solution to the [movie watchlist solo project on Scrimba's front end career path course](https://scrimba.com).
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Screenshot](#screenshot)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+    -   [What I learned](#what-i-learned)
+    -   [Continued development](#continued-development)
+-   [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+-   View the app on any device
+-   Search for movies by title
+-   Add movies to a watchlist
+-   Access the watchlist from a second page
+-   Access watchlist items from local storage
+
+### Screenshots
+
+![](/images/screenshot-1.png)
+![](/images/screenshot-2.png)
+![](/images/screenshot-3.png)
+![](/images/screenshot-4.png)
+
+### Links
+
+-   [Live Site URL](https://syd-movie-watchlist.netlify.app)
+
+## My process
+
+### Built with
+
+-   The [Open Movie Database](https://www.omdbapi.com/)
+-   Vanila HTML, CSS, And Javascript
+-   CSS Flexbox
+-   Mobile-first workflow
+
+### What I learned
+
+In JS, I learned I needed to use `Async Await` even when calling an Async function inside a loop:
+
+```js
+moviesList.forEach(async movie => {
+	const movieData = await getMovieData(movie.imdbID)
+	moviesListContainer.innerHTML += getMovieHtml(movieData, "add")
+})
+```
+
+This is how you select a child element:
+
+```js
+document.querySelector("parent > child")
+```
+
+In the CSS, I tried to have the movie plot on a separate line on smaller devices, and then move it next to the poster on larger device, but I found it too complicated. In the end, I decided to just have one layout, but making sure the poster won't be sqeezed when the viewport is too small.
+
+```css
+.movie-poster {
+	width: 20%;
+	align-self: flex-start; /* the magic line that helped keep the aspect ratio*/
+	border-radius: 3px;
+}
+```
+
+Other stuff
+
+-   Apparantly, you can't change an SVG's fill color using CSS if it's inserted directly in the HTML.
+-   The `indexOf()` method returns `-1` if it does not find anything (and not `undefined`.)
+
+### Continued development
+
+-   Add a _Next Page_ function to the search results
+-   Make the ap a PWA
+
+## Acknowledgments
+
+Header photo by <a href="https://unsplash.com/@felixmooneeram?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Felix Mooneeram</a> on <a href="https://unsplash.com/s/photos/cinema?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
---
+++ index.js
@@ -0,0 +1,126 @@
+const apikey = "295829c7"
+const searchBtn = document.getElementById("search-btn")
+const searchInputEl = document.getElementById("search-input")
+const moviesListContainer = document.getElementById("movies-list")
+const watchlistContainer = document.getElementById("watchlist")
+const watchlistPlaceholder = `
+    <p class="placeholder-text">Your watchlist is looking a little empty...<br />
+    <a href="/index.html" class="info-link">
+        <img src="/images/icon-add.svg" />
+        Let's add some movies!
+    </a></p>`
+
+let watchlistIds = JSON.parse(localStorage.getItem("watchlistIds")) || []
+
+searchBtn && searchBtn.addEventListener("click", handleSearch)
+
+document.addEventListener("click", e => {
+	e.target.dataset.add && addToWatchlist(e.target.dataset.add)
+	e.target.dataset.remove && removeFromWatchlist(e.target.dataset.remove)
+})
+
+async function handleSearch() {
+	const moviesData = await getSearchData(searchInputEl.value)
+	renderMoviesList(moviesData.Search)
+}
+
+function addToWatchlist(id) {
+	watchlistIds.push(id)
+	localStorage.setItem("watchlistIds", JSON.stringify(watchlistIds))
+	document.querySelector(`button[data-add="${id}"]`).disabled = true
+	document.querySelector(`button[data-add="${id}"] > img`).style.opacity = 0.2
+}
+
+function removeFromWatchlist(id) {
+	const index = watchlistIds.indexOf(id)
+	watchlistIds.splice(index, 1)
+	localStorage.setItem("watchlistIds", JSON.stringify(watchlistIds))
+	renderWatchlist(watchlistIds)
+}
+
+async function getSearchData(searchQuery) {
+	const searchResponse = await fetch(
+		`http://www.omdbapi.com/?s=${searchQuery}&type=movie&apikey=${apikey}`
+	)
+	const searchData = await searchResponse.json()
+	return searchData
+}
+
+async function getMovieData(movieId) {
+	const movieResponse = await fetch(
+		`http://www.omdbapi.com/?i=${movieId}&apikey=${apikey}`
+	)
+	const movieData = await movieResponse.json()
+	return movieData
+}
+
+function getMovieHtml(
+	{ Genre, Plot, Poster, Runtime, Title, Year, imdbID, imdbRating },
+	addRemove
+) {
+	return `
+        <section class="movie-item">
+            <img src="${Poster}" alt="Movie poster, ${Title}" class="movie-poster">
+            <div class="movie-info">
+                <h2 class="movie-title">
+                    ${Title}
+                    <small class="movie-year">(${Year})</small>
+                </h2>
+                <div class="movie-meta">
+                    <span class="movie-rating">
+                        <img src="/images/icon-star.svg" />
+                        ${imdbRating}
+                    </span>
+                    <span class="movie-runtime">${Runtime}</span>
+                    <span class="movie-genre">${Genre}</span>
+                    ${getBtnHtml(addRemove, imdbID)}
+                </div>
+                <p class="movie-plot">${Plot}</p>
+            </div>
+           
+            
+            
+        </section>
+    `
+}
+
+function getBtnHtml(type, id) {
+	let disabled = ""
+	let opacity = ""
+	if (type === "add" && watchlistIds.indexOf(id) !== -1) {
+		disabled = "disabled"
+		opacity = "style='opacity: 0.2'"
+	}
+	return `
+        <button
+            class="${type}-btn"
+            data-${type}="${id}"
+            ${disabled}
+        >
+            <img src="/images/icon-${type}.svg" ${opacity} />
+            ${type === "add" ? "Watchlist" : "Remove"}
+        </button>
+    `
+}
+
+function renderMoviesList(moviesList) {
+	moviesListContainer.innerHTML = ""
+	moviesList.forEach(async movie => {
+		const movieData = await getMovieData(movie.imdbID)
+		moviesListContainer.innerHTML += getMovieHtml(movieData, "add")
+	})
+}
+
+function renderWatchlist(movieIds) {
+	if (!movieIds.length) {
+		watchlistContainer.innerHTML = watchlistPlaceholder
+	} else {
+		watchlistContainer.innerHTML = ""
+		movieIds.forEach(async movieId => {
+			const movieData = await getMovieData(movieId)
+			watchlistContainer.innerHTML += getMovieHtml(movieData, "remove")
+		})
+	}
+}
+
+watchlistContainer && renderWatchlist(watchlistIds)
